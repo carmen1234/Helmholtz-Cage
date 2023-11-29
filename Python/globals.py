7e9f5bcc378343982ebe6c1e4aaa9b6f715a3ec2
RudainaK <rkhalil828@gmail.com>
2023-11-29 12:16:08 -0600

    added util function that should convert raw_mag val to gauss

--- Python/globals.py
+++ Python/globals.py
@@ -1,6 +1,8 @@
 # Hold all global configuration and data variables here
 # Can be imported into other files using `from globals import ...`
 
+from enum import Enum
+
 # Config
 port = "/dev/cu.usbmodem21201"
 
@@ -9,3 +11,10 @@ port = "/dev/cu.usbmodem21201"
 sensor_data = {"current": 0.0, # float
                "magnetic_field": 0.0 # float
               }
+
+# HW parameters
+class HW_params(float, Enum):
+    raw_mag_min = -32768.0
+    raw_mag_max = 32767.0
+    min_gauss = -8.0
+    max_guass = 8.0
--- Python/util.py
+++ Python/util.py
@@ -1,5 +1,5 @@
 import csv
-
+from globals import HW_params
 
 def read_csv_into_dict(csv_path):
     csv_file = open(csv_path,"r")
@@ -8,9 +8,15 @@ def read_csv_into_dict(csv_path):
 def get_current_from_mag_field(mag_fielld):
     # using B = u*n*I
     #u of copper = 1.257x10^(-6) from wikipedia
-    n = 6 #for demo on nov 24
+    n = 5 #for demo on nov 24
 
     I = mag_fielld/(pow(1.257,-6)*n)
+    return I
+
+def convert_raw_mag_input_to_gauss(raw_mag_data):
+    mag_val = (((max_guass - min_gauss)*(raw_mag_data - raw_mag_min))/(raw_mag_max - raw_mag_min)) + raw_mag_min
+    return mag_val
+
 
 
 
