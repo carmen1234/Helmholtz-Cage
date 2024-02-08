520d7bc25965b9353419420b4a3a70e36457c079
RudainaK <rkhalil828@gmail.com>
2024-02-08 13:08:09 -0600

    made fixes regarding comments

--- Python/control.py
+++ Python/control.py
@@ -1,42 +1,31 @@
-
-# consts (tune) (should I move these to globals.py?)
-epsilon = 0.000001
-Kp = 1
-Kd = 1
-Ki = 1
-
-def proportional(setpoint,process_var):
-    err = setpoint - process_var
-    return (Kp*err)
-    
-
-
-def differential(setpoint,process_var,time_interval):
-    err = setpoint - process_var
-    err_diff = err/time_interval
-    return Kd*err_diff
-
-
-
-def integral(setpoint,process_var,time_interval):
-    err = setpoint - process_var
-    err_integ = err*time_interval
-    return Ki*err_integ
-
-
-
-
-
 class PID_controller:
-    def __init__(setpoint,process_var,time_interval):
+    def __init__(setpoint,process_var,time_interval, epsilon, Kp, Kd, Ki):
         self.setpoint = setpoint
         self.process_var = process_var
         self.time_interval = time_interval
+        self.epsilon = epsilon
+        self.Kp = Kp
+        self.Kd = Kd
+        self.Ki = Ki
     
     def update_values(setpoint,process_var,time_interval):
         self.setpoint = setpoint
         self.process_var = process_var
         self.time_interval = time_interval
+
+    def proportional(setpoint,process_var):
+        err = setpoint - process_var
+        return (Kp*err)     
+
+    def differential(setpoint,process_var,time_interval):
+        err = setpoint - process_var
+        err_diff = err/time_interval
+        return Kd*err_diff
+
+    def integral(setpoint,process_var,time_interval):
+        err = setpoint - process_var
+        err_integ = err*time_interval
+        return Ki*err_integ
 
     def get_PID(self):
         return (proportional(self.setpoint,self.process_var,self.time_interval) 
--- Python/util.py
+++ Python/util.py
@@ -5,12 +5,12 @@ def read_csv_into_dict(csv_path):
     csv_file = open(csv_path,"r")
     return csv.DictReader(csv_file) #default delim is ","
 
-def get_current_from_mag_field(mag_fielld):
+def get_current_from_mag_field(mag_field):
     # using B = u*n*I
     #u of copper = 1.257x10^(-6) from wikipedia
     n = 5 #for demo on nov 24
 
-    I = mag_fielld/(pow(1.257,-6)*n)
+    I = mag_field/(pow(1.257,-6)*n)
     return I
 
 def convert_raw_mag_input_to_gauss(raw_mag_data):
